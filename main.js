cca6a0300faa371f6eb1794f6e7bc4ef8e40cf23
EvanHendersonRichtByte <adamhenderson3x3@gmail.com>
2022-02-07 15:47:31 +0700

    update file directory parameter

--- main.js
+++ main.js
@@ -2,20 +2,26 @@
 var c = console.log;
 var fs = require("fs");
 var path = require("path");
-var files = fs.readdirSync("./modules/");
 var filename;
+var fileDirectory;
 var includedFn = [];
-var include = function (files, includedFn) {
+var files;
+var importedFiles;
+var include = function (files, fileDirectory, includedFn) {
+    if (fileDirectory === void 0) { fileDirectory = "./modules/"; }
+    files = fs.readdirSync(fileDirectory);
+    importedFiles = files;
     files.forEach(function (item, index) {
         filename = item.replace(/\.[^/.]+$/, "");
         if (item.endsWith(".js")) {
             includedFn[index] = filename;
-            global[filename] = require(path.join(__dirname, "modules", item));
+            global[filename] = require(path.join(__dirname, fileDirectory, item));
         }
     });
 };
 var functionList = function () { return includedFn; };
-var fileList = function () { return files; };
+var fileList = function () { return importedFiles; };
 module.exports = { include: include, functionList: functionList, fileList: fileList };
-include(files, includedFn);
-c(includedFn);
+// include(files, "./others/", includedFn);
+// c(functionList());
+// c(fileList());
--- main.ts
+++ main.ts
@@ -3,24 +3,33 @@ const c = console.log;
 
 const fs = require("fs");
 const path = require("path");
-let files = fs.readdirSync("./modules/");
 let filename: string;
+let fileDirectory: string;
 let includedFn = [];
+let files: string[];
+let importedFiles;
 
-const include = (files: string[], includedFn: string[]) => {
+const include = (
+  files: string[],
+  fileDirectory = "./modules/",
+  includedFn: string[]
+) => {
+  files = fs.readdirSync(fileDirectory);
+  importedFiles = files;
   files.forEach((item, index) => {
     filename = item.replace(/\.[^/.]+$/, "");
     if (item.endsWith(".js")) {
       includedFn[index] = filename;
-      global[filename] = require(path.join(__dirname, "modules", item));
+      global[filename] = require(path.join(__dirname, fileDirectory, item));
     }
   });
 };
 
 const functionList = () => includedFn;
-const fileList = () => files;
+const fileList = () => importedFiles;
 
 module.exports = { include, functionList, fileList };
 
-include(files, includedFn);
-c(includedFn);
+// include(files, "./others/", includedFn);
+// c(functionList());
+// c(fileList());
